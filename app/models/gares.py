28d82421db1c61ce245db92f82a912e8d521c361
Melina-Conter <ninimc33@gmail.com>
2025-03-28 18:28:35 +0100

    Bonnes pratiques des codes : - docstring et commentaires sur tous les fichiers - suprresion des  anciens fichiers non utilisés

--- app/models/gares.py
+++ app/models/gares.py
@@ -1,9 +1,41 @@
 from ..app import app, db
 from sqlalchemy import ForeignKey
-from werkzeug.security import generate_password_hash
 from sqlalchemy.sql import func
 
 class Gares(db.Model):
+    """
+    Une classe pour représenter la table gares de la base de données.
+
+    Attributs
+    ---------
+    UIC : sqlalchemy.sql.schema.Column
+        Identifiant unique de la gare (clé primaire).
+    nom : sqlalchemy.sql.schema.Column
+        Nom de la gare.
+    latitude : sqlalchemy.sql.schema.Column
+        Latitude de la gare.
+    longitude : sqlalchemy.sql.schema.Column
+        Longitude de la gare.
+    adresse : sqlalchemy.sql.schema.Column
+        Adresse de la gare.
+    commune : sqlalchemy.sql.schema.Column
+        Commune où se situe la gare.
+    code_postal : sqlalchemy.sql.schema.Column
+        Code postal de la gare.
+    moyenne_frequentation_2021_2023 : sqlalchemy.sql.schema.Column
+        Moyenne de fréquentation de la gare.
+    region : sqlalchemy.sql.schema.Column
+        Région où se situe la gare.
+
+    favorites : sqlalchemy.orm.relationship
+        Relation avec la table Gares_favorites.
+    horaires : sqlalchemy.orm.relationship
+        Relation avec la table Horaires.
+    trouves : sqlalchemy.orm.relationship
+        Relation avec la table Objets_trouves.
+    declarations_pertes : sqlalchemy.orm.relationship
+        Relation avec la table Declaration_de_perte.
+    """
     __tablename__="gares"
     UIC=db.Column(db.Integer, primary_key=True)
     nom=db.Column(db.Text)
@@ -22,6 +54,22 @@ class Gares(db.Model):
     declarations_pertes=db.relationship("Declaration_de_perte", backref="gares", lazy=True)
 
 class Horaires(db.Model):
+    """
+    Une classe qui représente la table horaires de la base de données.
+
+    Attributs
+    ---------
+    id : sqlalchemy.sql.schema.Column
+        Identifiant unique de l'horaire (clé primaire).
+    UIC : sqlalchemy.sql.schema.Column
+        Identifiant unique de la gare (clé étrangère).
+    jour : sqlalchemy.sql.schema.Column
+        Jour de la semaine.
+    horaire_jour_normal : sqlalchemy.sql.schema.Column
+        Horaire pour un jour normal.
+    horaire_jour_ferie : sqlalchemy.sql.schema.Column
+        Horaire pour un jour férié.
+    """
     __tablename__="horaires"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UIC=db.Column(db.Integer, ForeignKey('gares.UIC')) #Foreign Key UIC de Gares
@@ -30,6 +78,24 @@ class Horaires(db.Model):
     horaire_jour_ferie=db.Column(db.String(30))
 
 class Objets_trouves(db.Model):
+    """
+    Une classe qui représente la table objets_trouves de la base de données.
+
+    Attributs
+    ---------
+    id : sqlalchemy.sql.schema.Column
+        Identifiant unique de l'objet trouvé (clé primaire).
+    UIC : sqlalchemy.sql.schema.Column
+        Identifiant unique de la gare (clé étrangère).
+    date_perte : sqlalchemy.sql.schema.Column
+        Date de la perte de l'objet.
+    date_restitution : sqlalchemy.sql.schema.Column
+        Date de la restitution de l'objet.
+    type_objet : sqlalchemy.sql.schema.Column
+        Type de l'objet trouvé.
+    nature_objet : sqlalchemy.sql.schema.Column
+        Nature de l'objet trouvé.
+    """
     __tablename__="objets_trouves"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UIC=db.Column(db.Integer, ForeignKey('gares.UIC')) #Foreign Key UIC de Gares
@@ -39,6 +105,20 @@ class Objets_trouves(db.Model):
     nature_objet=db.Column(db.Text)
 
 class Declaration_de_perte(db.Model):
+    """
+    Une classe qui représente la table declarations_de_perte de la base de données.
+
+    Attributs
+    ---------
+    id : sqlalchemy.sql.schema.Column
+        Identifiant unique de la déclaration de perte (clé primaire).
+    UIC : sqlalchemy.sql.schema.Column
+        Identifiant unique de la gare (clé étrangère).
+    date_perte : sqlalchemy.sql.schema.Column
+        Date de la perte de l'objet.
+    type_objet : sqlalchemy.sql.schema.Column
+        Type de l'objet perdu.
+    """
     __tablename__="declarations_de_perte"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UIC=db.Column(db.Integer, ForeignKey('gares.UIC')) #Foreign Key UIC de Gares
